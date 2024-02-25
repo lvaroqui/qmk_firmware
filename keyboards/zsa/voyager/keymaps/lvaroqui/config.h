1b6f4ec3d19ffdcd5b3b5391df4dabb54ec6405a
Luc Varoqui <luc@varoqui.org>
2024-02-25 16:45:15 +0100

    begin custom layer

---
+++ keyboards/zsa/voyager/keymaps/lvaroqui/config.h
@@ -0,0 +1,9 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM_PER_KEY
+#define PERMISSIVE_HOLD
---
+++ keyboards/zsa/voyager/keymaps/lvaroqui/keymap.c
@@ -0,0 +1,120 @@
+#include QMK_KEYBOARD_H
+#include "keymap_french.h"
+#include "sendstring_french.h"
+#include "process_key_override.h"
+
+#include "features/achordion.h"
+
+// TODO
+// - special key on all layers
+// - symbol layers
+// - top row
+
+enum {
+    HOME_PLUS = SAFE_RANGE,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT(
+    C(FR_Z),            FR_1,           KC_2,         KC_3,            KC_4,              KC_5,      /* */        KC_6,           FR_UNDS,        KC_8,           KC_9,           KC_0,           KC_MINUS,
+    KC_TAB,             FR_AGRV,        FR_J,         LT(0, FR_O),     FR_EACU,           FR_B,      /* */        FR_F,           FR_D,           FR_L,           FR_QUOT,        FR_Q,           FR_X,
+    CW_TOGG,            LGUI_T(FR_A),   LALT_T(FR_I), LSFT_T(FR_E),    LCTL_T(FR_U),      FR_COMM,   /* */        FR_P,           RCTL_T(FR_T),   RSFT_T(FR_S),   LALT_T(FR_R),   RGUI_T(FR_N),   FR_CIRC,
+    C(FR_Z),            FR_K,           LT(0, FR_Y),  LT(0, FR_EGRV),  LT(0, FR_DOT),     FR_W,      /* */        FR_G,           FR_C,           FR_M,           FR_H,           FR_V,           FR_Z,
+                                                                        LT(1, KC_ENTER), KC_ESC,     /* */        LT(2, KC_BACKSPACE), LT(2, KC_SPACE)
+  ),
+  [1] = LAYOUT(
+    KC_F1,          KC_F2,           KC_F3,           KC_F4,            KC_F5,            KC_F6,     /* */        KC_F7,          KC_F8,          KC_F9,          KC_F10,          KC_F11,         KC_F12,
+    KC_TRANSPARENT, FR_QUOT,         FR_LABK,         FR_RABK,          FR_DQUO,          FR_DOT,    /* */        FR_AMPR,        FR_SCLN,        FR_LBRC,        FR_RBRC,         FR_PERC,        KC_NO,
+    KC_TRANSPARENT, LGUI_T(FR_EXLM), LALT_T(FR_MINS), HOME_PLUS,        LCTL_T(FR_EQL),   FR_HASH,   /* */        FR_PIPE,        FR_COLN,        FR_LPRN,        FR_RPRN,         FR_QUES,        KC_BACKSPACE,
+    KC_TRANSPARENT, FR_CIRC,         FR_SLSH,         FR_ASTR,          FR_BSLS,          KC_NO,     /* */        FR_TILD,        FR_DLR,         FR_LCBR,        FR_RCBR,         FR_AT,          KC_ENTER,
+                                                                   KC_TRANSPARENT, KC_TRANSPARENT,   /* */        KC_TRANSPARENT, KC_TRANSPARENT
+  ),
+  [2] = LAYOUT(
+    RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MOD,        KC_TRANSPARENT,        RGB_VAD,        RGB_VAI,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  KC_TRANSPARENT,                                 KC_PGUP,        KC_HOME,        KC_UP,          KC_END,         KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, LGUI_T(KC_MEDIA_PREV_TRACK),LALT_T(KC_MEDIA_NEXT_TRACK),LSFT_T(KC_MEDIA_STOP),  LCTL_T(KC_MEDIA_PLAY_PAUSE),KC_TRANSPARENT,                                 KC_PAGE_DOWN,      KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT,                                KC_TRANSPARENT, LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
+  ),
+};
+// clang-format on
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &ko_make_basic(MOD_MASK_SHIFT, FR_DOT, FR_COLN),                //
+    &ko_make_basic(MOD_MASK_SHIFT, FR_COMM, FR_SCLN),               //
+    &ko_make_basic(MOD_MASK_SHIFT, FR_QUOT, FR_QUES),               //
+    &ko_make_basic(MOD_MASK_SHIFT, FR_CIRC, FR_EXLM),               //
+    &ko_make_basic(MOD_MASK_SHIFT, LT(2, KC_BACKSPACE), KC_DELETE), //
+    NULL                                                            // Null terminate the array of overrides!
+};
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(2, KC_BACKSPACE):
+        case LT(2, KC_SPACE):
+            return 200;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_achordion(keycode, record)) {
+        return false;
+    }
+
+    static uint16_t home_plus_timer = 0;
+
+    switch (keycode) {
+        case LT(0, FR_Y):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(FR_X)); // Intercept hold function to send Ctrl-X
+                return false;
+            }
+            return true;
+        case LT(0, FR_EGRV):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(FR_C)); // Intercept hold function to send Ctrl-C
+                return false;
+            }
+            return true;
+        case LT(0, FR_DOT):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(FR_V)); // Intercept hold function to send Ctrl-V
+                return false;
+            }
+            return true;
+        case HOME_PLUS:
+            if(record->event.pressed) {
+                home_plus_timer = timer_read();
+                register_code(KC_LSFT);
+            } else {
+                unregister_code(KC_LSFT);
+                if (timer_elapsed(home_plus_timer) < TAPPING_TERM) {
+                    SEND_STRING("+");
+                }
+            }
+            return false; // We handled this keypress
+    }
+
+    return true;
+}
+
+void matrix_scan_user(void) {
+    achordion_task();
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record, uint16_t other_keycode, keyrecord_t *other_record) {
+    return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+    switch (tap_hold_keycode) {
+        case LT(1, KC_ENTER):
+        case LT(2, KC_SPACE):
+            return 0; // Bypass Achordion for these keys.
+    }
+
+    return 800; // Otherwise use a timeout of 800 ms.
+}
